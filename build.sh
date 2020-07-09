aa2c7a5f40df4b8de7a4ac802134c883137f8847
Aleksandar Milosevic <dev@sasxa.net>
2020-07-09 12:56:24 +0300

    remove _redirects

--- build.sh
+++ build.sh
@@ -7,5 +7,4 @@ cp src/assets/* public/$project/
 
 cp src/index.html public/$project/
 cp src/sw.js public/$project/
-cp src/_redirects public/$project/
 cp src/webmanifest-$project.webmanifest public/$project/site.webmanifest
