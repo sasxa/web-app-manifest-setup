b00d90a47ba6efd3b6ee3db5c1135ae81c4bfe7e
Aleksandar Milosevic <dev@sasxa.net>
2020-07-09 12:47:39 +0300

    add _redirects file

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ cp src/assets/* public/$project/
 
 cp src/index.html public/$project/
 cp src/sw.js public/$project/
+cp src/_redirects public/$project/
 cp src/webmanifest-$project.webmanifest public/$project/site.webmanifest
