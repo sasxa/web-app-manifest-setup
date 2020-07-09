dfc1c4f282d464afae3987818e3b8bb4bf3aefba
Aleksandar Milosevic <dev@sasxa.net>
2020-07-09 12:36:36 +0300

    test service worker scope

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 project=$1
 shift
 
-mkdir -p public/$project/worker
+mkdir -p public/$project
 
 cp src/assets/* public/$project/
 
 cp src/index.html public/$project/
-cp src/sw.js public/$project/worker/
+cp src/sw.js public/$project/
 cp src/webmanifest-$project.webmanifest public/$project/site.webmanifest
